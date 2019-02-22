985512a6d93017c2fbf4a42a4fe24bda443a53a0
Fabian <cmdrf@users.noreply.github.com>
2019-02-22 10:12:43 +0100

    Update libsigrokdecode to 0.5.2

--- External_libsigrokdecode.cmake
+++ External_libsigrokdecode.cmake
@@ -2,8 +2,8 @@ ExternalProject_Add(libsigrokdecode
 	DEPENDS pkg-config
 	DEPENDS libsigrok
 	DEPENDS python
-	URL http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.5.1.tar.gz
-	URL_MD5 1e950dfb4c521a334e150640348efcd4
+	URL http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.5.2.tar.gz
+	URL_MD5 b9033bc7e68bc17fffffd4fdd793f5a1
 	BUILD_IN_SOURCE On
 	CONFIGURE_COMMAND env
 		PKG_CONFIG=${PKG_CONFIG_EXECUTABLE}
